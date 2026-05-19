95815318a6e6ef804889d85df21556d2f6c228ee
unknown <zeyneptatar71@gmail.com>
2026-05-19 19:02:54 +0300

    son

---
+++ nutriscan-backend/nutriscan/src/main/java/com/nutriscan/nutriscan/NutriscanApplication.java
@@ -0,0 +1,13 @@
+package com.nutriscan.nutriscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriscanApplication.class, args);
+	}
+
+}
---
+++ nutriscan-backend/nutriscan/src/test/java/com/nutriscan/nutriscan/NutriscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutriscan.nutriscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
